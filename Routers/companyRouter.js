19c8a8eb3e31beb6378598128becdabe19554103
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-04-28 13:23:47 +0530

    Update companyRouter.js 1:23

--- Routers/companyRouter.js
+++ Routers/companyRouter.js
@@ -5,14 +5,14 @@ import { authMiddleware } from "../Middlewares/Auth.js";
 
 const router = express.Router();
 
-router.post("/company",  companyController.createCompany);
+router.post("/company", companyController.createCompany);
 router.get("/getallCompany", companyController.getallCompany);
 router.get("/getCompanyById/:id", companyController.getCompanyById);
 // router.post("/business", QRCodeontroller.createBusiness);
 // router.put("/qr-code/:id", authMiddleware, QRCodeontroller.editQRCode);
 router.delete("/deleteCompany/:id", companyController.deleteCompany);
 router.post("/updateCompanyStatus/:id", companyController.updateCompanyStatus);
-
+router.put("/editCompany/:id", companyController.editCompany)
 router.get("/getCompanyDetails", companyController.getCompanyDetails);
 router.get("/getCompanyDetailsforSentimentAnalytics", companyController.getCompanyDetailsforSentimentAnalytics);
 // router.get("/getCompanyHeadlineDetails", companyController.getCompanyHeadlineDetails);
