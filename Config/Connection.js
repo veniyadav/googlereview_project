b0f35af424093f8cdd906d8525f42167b443c770
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-05-12 17:47:28 +0530

    connection

--- Config/Connection.js
+++ Config/Connection.js
@@ -1,47 +1,16 @@
-//local db
-import mysql from "mysql2";
-
-const dbConfig = {
-  host: process.env.HOSTNAME,
-  port: process.env.DBPORT,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "google-review",
-  charset: "utf8mb4",
-  multipleStatements: false,
-  timezone: "Z",
-
-};
-
-const pool = mysql.createPool(dbConfig);
-
-pool.promise()
-  .getConnection()
-  .then((connection) => {
-    console.log("Database connected successfully");
-    connection.release(); 
-  })
-  .catch((err) => {
-    console.error("Error connecting to the database:", err.message);
-  });
-
-export default pool.promise();
-
-
-// live db
-
-
+// //local db
 // import mysql from "mysql2";
 
 // const dbConfig = {
-//   host: "tramway.proxy.rlwy.net",
-//   port: 43491,
+//   host: process.env.HOSTNAME,
+//   port: process.env.DBPORT,
 //   user: "root",
-//   password: "EBhJaMfFUSXyyMuXGUrXJEikHRWXNuwM",
-//   database: "railway",
+//   password: process.env.PASSWORD,
+//   database: "google-review",
 //   charset: "utf8mb4",
 //   multipleStatements: false,
 //   timezone: "Z",
+
 // };
 
 // const pool = mysql.createPool(dbConfig);
@@ -50,10 +19,41 @@ export default pool.promise();
 //   .getConnection()
 //   .then((connection) => {
 //     console.log("Database connected successfully");
-//     connection.release();
+//     connection.release(); 
 //   })
 //   .catch((err) => {
 //     console.error("Error connecting to the database:", err.message);
 //   });
 
 // export default pool.promise();
+
+
+// live db
+
+
+import mysql from "mysql2";
+
+const dbConfig = {
+  host: "tramway.proxy.rlwy.net",
+  port: 43491,
+  user: "root",
+  password: "EBhJaMfFUSXyyMuXGUrXJEikHRWXNuwM",
+  database: "railway",
+  charset: "utf8mb4",
+  multipleStatements: false,
+  timezone: "Z",
+};
+
+const pool = mysql.createPool(dbConfig);
+
+pool.promise()
+  .getConnection()
+  .then((connection) => {
+    console.log("Database connected successfully");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err.message);
+  });
+
+export default pool.promise();
